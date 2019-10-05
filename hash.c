82b875f9afdf6d869ea545de0ebb1173a4a121a8
AgussAndrade <51175036+AgussAndrade@users.noreply.github.com>
2019-10-05 19:29:37 -0300

    Update hash.c

--- hash.c
+++ hash.c
@@ -84,3 +84,15 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
 	hash->arr = arreglo;
 	return hash;
 }
+size_t hash(char const *input) { 
+
+    const int ret_size = 32;
+    size_t ret = 0x555555;
+    const int per_char = 7;
+
+    while (*input) { 
+        ret ^= *input++;
+        ret = ((ret << per_char) | (ret >> (ret_size - per_char));
+   }
+   return ret;
+}
