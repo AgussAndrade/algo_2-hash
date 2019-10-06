75f716ff498b656e4704ab10c518dd5f2b238f9c
AgussAndrade <51175036+AgussAndrade@users.noreply.github.com>
2019-10-06 19:06:41 -0300

    Add files via upload

--- hash.c
+++ hash.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <limits.h>
+#include <stdlib.h>
 
 #define LARGO_I 31
 
@@ -15,7 +16,7 @@ typedef struct campo{
 struct hash{
 	size_t capacidad;
 	size_t cantidad;
-	hash_destruir_dato_t;
+	hash_destruir_dato_t* destruir;
 	campo_t* arr;
 };
 
@@ -23,16 +24,33 @@ struct hash_iter{
 	hash_t* hash;
 	campo_t* act;
 	size_t pos; //posicion de donde estoy, si estoy en > a capacidad me pase y termine de iterar 
+};
+
+bool hash_redimensionar (hash_t* hash){
+	campo_t* datos_nuevo = malloc(hash->capacidad * 2 * sizeof(campo_t));
+	if (datos_nuevo == NULL){
+		return false;
+	}
+	campo_t* aux = hash->arr;
+	hash->arr = datos_nuevo;
+	int j = hash->capacidad;
+	hash->capacidad *=2;
+	hash->cantidad = 0;
+	for(int i = 0; i < j;i++){
+		if(aux[i] != NULL){
+			if(aux[i]->estado){
+				free(aux[i]);
+			}
+			else{
+				hash_guardar(aux[i]->clave,aux[i]->valor);
+				free(aux[i]);
+			}
+		}
+	}
+	free(aux);
+	return true;
 }
 
-// unsigned int hashing(const char *word) {
-//     unsigned int hash = 0, c;
-//     for (size_t i = 0; word[i] != '\0'; i++) {
-//         c = (unsigned char)word[i];
-//         hash = (hash << 3) + (hash >> (sizeof(hash) * CHAR_BIT - 3)) + c;
-//     }
-//     return hash % SIZE;
-// }
 int hashing (const char* word)
 {
     unsigned int hash = 0;
@@ -42,6 +60,7 @@ int hashing (const char* word)
     }
     return hash % SIZE;
 }
+
 int hash_buscar(hash_t* hash,char* clave){
 	int pos = hashing(clave);
 	int i = pos;
@@ -84,15 +103,44 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
 	hash->arr = arreglo;
 	return hash;
 }
-size_t hash(char const *input) { 
 
-    const int ret_size = 32;
-    size_t ret = 0x555555;
-    const int per_char = 7;
+bool hash_guardar(hash_t *hash, const char *clave, void *dato){
+	if((hash->cantidad + 1) *10 / hash->capacidad >= 7){
+		if(!hash_redimensionar(hash)) return false;
+	}
+	int pos = hash_buscar(hash,clave);
 
-    while (*input) { 
-        ret ^= *input++;
-        ret = ((ret << per_char) | (ret >> (ret_size - per_char));
-   }
-   return ret;
+	if(hash->arr[pos] == NULL){
+		campo_t* campo = malloc(sizeof(campo_t));
+		if(!campo) return false;
+		campo->clave = clave;
+		campo->dato = dato;
+		campo->estado = false;
+	}
+	else{
+		hash->destruir(hash->arr[pos]->valor);
+		hash->arr[pos]->valor = dato;
+		hash->arr[pos]->estado = false;
+	}
+	hash->cantidad++;
+	return true;
 }
+
+void *hash_obtener(const hash_t *hash, const char *clave){
+	int pos = hash_buscar(hash,clave);
+	if(hash->arr[pos] == NULL){
+		return NULL;
+	}
+	else if (hash->arr[pos]->estado){
+		return NULL;
+	}
+	return hash->arr[pos]->valor;
+}
+void *hash_obtener(const hash_t *hash, const char *clave){
+	if (hash_obtener(hash,clave)!= NULL){
+		return true;
+	}
+	return false;
+}
+
+
