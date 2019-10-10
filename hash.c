634a21b05e6a57e72a1b06b80c3cceb4831e2e92
AgussAndrade <51175036+AgussAndrade@users.noreply.github.com>
2019-10-09 22:09:20 -0300

    Add files via upload

--- hash.c
+++ hash.c
@@ -1,10 +1,10 @@
 #include "hash.h"
-
+#include "testing.h"
 #include <stdbool.h>
 #include <stddef.h>
 #include <limits.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 #define LARGO_I 37
 
 typedef enum {vacio,ocupado,borrado} estado_t;
@@ -19,32 +19,53 @@ struct hash{
 	size_t capacidad;
 	size_t cantidad;
 	hash_destruir_dato_t destruir;
-	campo_t* arr;
+	campo_t** arr;
 };
 
 struct hash_iter{
-	hash_t* hash;
+	const hash_t* hash;
 	campo_t* act;
 	size_t pos; //posicion de donde estoy, si estoy en == a capacidad me pase y termine de iterar 
 };
 
-bool hash_redimensionar (hash_t* hash){
-	campo_t* datos_nuevo = malloc(hash->capacidad * 2 * sizeof(campo_t));
+void destruir_arr(campo_t** arr, size_t largo){
+	int i ;
+	for(i =0;i < (int)largo;i++){
+		free(arr[i]);
+	}
+	free(arr);
+}
+bool pedir_campos(campo_t** arreglo,size_t tam){
+	for(int i = 0 ; i < tam;i++){
+		campo_t* aux = malloc(sizeof(campo_t));
+		if(!aux){
+			destruir_arr(arreglo,tam);
+			return false;
+		}
+		arreglo[i] = aux;
+		arreglo[i]->estado = vacio;
+		arreglo[i]->valor = NULL;
+	}
+	return true;
+}
+bool hash_redimensionar (hash_t* hash,size_t tam){
+	campo_t** datos_nuevo = malloc(tam * sizeof(campo_t*));
 	if (datos_nuevo == NULL){
 		return false;
 	}
+	if(!pedir_campos(datos_nuevo,tam)) return false;
 
-	campo_t* aux = hash->arr;
+	campo_t** aux = hash->arr;
 	hash->arr = datos_nuevo;
 	int j = hash->capacidad;
-	hash->capacidad *=2;
+	hash->capacidad = tam;
 	hash->cantidad = 0;
-
 	for(int i = 0; i < j;i++){
 		
-		if(aux[i].estado == ocupado){
-			hash_guardar(hash,aux[i].clave,aux[i].valor);
+		if(aux[i]->estado == ocupado){
+			hash_guardar(hash,aux[i]->clave,aux[i]->valor);
 		}
+		free(aux[i]);
 	}
 	free(aux);
 	return true;
@@ -61,19 +82,18 @@ int hashing (const char* word)
     return hash;
 }
 
-int hash_buscar(const hash_t* hash,const char* clave){
-	int pos = hashing(clave)%hash->capacidad;
-	int i = pos;
+size_t hash_buscar(const hash_t* hash,const char* clave){
+	size_t pos = (size_t)hashing(clave) % hash->capacidad;
+	size_t i = pos;
 	while (true){
-		if(hash->arr[i].estado == vacio){
+		// printf("%i %i %i\n",i,pos, hash->capacidad );
+		if(hash->arr[i]->estado == vacio){
 			return i;
 		}
-		else{
-			if (hash->arr[i].clave == clave){
-				return i;
-			}
+		else if (hash->arr[i]->clave == clave){
+			return i;
 		}
-		if( i >= hash->capacidad - 1){
+		if( i == hash->capacidad - 1){
 			i = 0;
 			continue;
 		}
@@ -82,7 +102,7 @@ int hash_buscar(const hash_t* hash,const char* clave){
 			break;
 		}
 	}
-	return -1; //es imposible que devuelva -1 por el tema de que nunca se llena pero sin esto no compila
+	return pos; //es imposible que devuelva esto por el tema de que nunca se llena pero sin esto no compila
 }
 
 hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
@@ -93,60 +113,60 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
 	hash->cantidad = 0;
 	hash->destruir = destruir_dato;
 	
-	campo_t* arreglo = malloc(sizeof(campo_t)*LARGO_I);
+	campo_t** arreglo = malloc(sizeof(campo_t*)*LARGO_I);
 	
 	if (!arreglo){
 		free(hash);
 		return NULL;
 	}
 	
-	for(int i = 0 ; i < LARGO_I;i++){
-		arreglo[i].estado = vacio;
-		arreglo[i].valor = NULL;
-	}
-	
+	if(!pedir_campos(arreglo,LARGO_I)) return NULL;	
 	hash->arr = arreglo;
 	return hash;
 }
 
 bool hash_guardar( hash_t *hash, const char *clave, void *dato){
 	if((hash->cantidad + 1) *10 / hash->capacidad >= 7){
-		if(!hash_redimensionar(hash)) return false;
+		if(!hash_redimensionar(hash,(hash->capacidad)*2 )) return false;
 	}
 	const hash_t* A = hash;
-	int pos = hash_buscar(A,clave);
+	size_t pos = hash_buscar(A,clave);
 	// if (posicion == -1){
 	// 	return false;
 	// } 
 
-	if(hash->arr[pos].estado == vacio ){
-		hash->arr[pos].clave = clave;
-		hash->arr[pos].valor = dato;
-		hash->arr[pos].estado = ocupado;
+	if(hash->arr[pos]->estado == vacio ){
+		hash->arr[pos]->clave = clave;
+		hash->arr[pos]->valor = dato;
+		hash->arr[pos]->estado = ocupado;
+		hash->cantidad++;
 	}
 	else{ 
 /* hash buscar mira solo por clave, si borraste una clave y la queres volver a guardar tambien sirve */
 		if(hash->destruir != NULL){
-			hash->destruir(hash->arr[pos].valor); 
+			hash->destruir(hash->arr[pos]->valor); 
 		}
-		hash->arr[pos].valor = dato;
-		hash->arr[pos].estado = ocupado;
+		hash->arr[pos]->valor = dato;
+		if(hash->arr[pos]->estado == borrado){
+			hash->cantidad++;
+		}
+		hash->arr[pos]->estado = ocupado;
 	}
-	hash->cantidad++;
+	
 	return true;
 }
 
 void *hash_obtener(const hash_t *hash, const char *clave){
 	int pos = hash_buscar(hash,clave);
-	if(hash->arr[pos].estado != ocupado){
+	if(hash->arr[pos]->estado != ocupado){
 		return NULL;
 	}
-	return hash->arr[pos].valor;
+	return hash->arr[pos]->valor;
 }
 
 bool hash_pertenece(const hash_t *hash, const char *clave){
 	int pos = hash_buscar(hash,clave);
-	if (hash->arr[pos].estado == ocupado){
+	if (hash->arr[pos]->estado == ocupado){
 		return true;
 	}
 	return false;
@@ -157,29 +177,40 @@ size_t hash_cantidad(const hash_t *hash){
 }
 
 void *hash_borrar(hash_t *hash, const char *clave){
-	int pos = hashing(clave);
-	if (hash->arr[pos].estado != ocupado){
+	size_t pos = hash_buscar(hash,clave);
+	// printf("%i %i\n",pos, hash->capacidad );
+	if (hash->arr[pos]->estado != ocupado){
 		return NULL;
 	}
-	hash->arr[pos].estado = borrado;
-	void* rta = hash->arr[pos].valor;
-	hash->arr[pos].valor = NULL;
-	hash->cantidad --;
+	if ((hash->capacidad >= LARGO_I) && ((hash->capacidad)*10/hash->cantidad <= 1)){
+		if (hash_redimensionar(hash, hash->capacidad/2)){}
+		else{
+			return NULL;
+		}
+	}
+	
+	hash->arr[pos]->estado = borrado;
+	void* rta = hash->arr[pos]->valor;
+	hash->arr[pos]->valor = NULL;
+	if(hash->cantidad >0){
+		hash->cantidad --;
+	}
 	return rta;
 }
 void hash_destruir(hash_t *hash){
-	campo_t* arreglo = hash->arr;
+	campo_t** arreglo = hash->arr;
 	if(hash->destruir != NULL){
 		for(int i =0; i<hash->capacidad;i++){
-			if (arreglo[i].estado == ocupado){
-				hash->destruir(arreglo[i].valor);
+			if (arreglo[i]->estado == ocupado){
+				hash->destruir(arreglo[i]->valor);
 			}
+			free(arreglo[i]);
 		}
 	}
 	free(arreglo);
 	free(hash);
 }
-/ Crea iterador
+// Crea iterador
 hash_iter_t* hash_iter_crear(const hash_t* hash){
 	hash_iter_t* iter = malloc(sizeof(hash_iter_t));
 	if (iter == NULL){
@@ -191,7 +222,7 @@ hash_iter_t* hash_iter_crear(const hash_t* hash){
 		iter->pos = 0;
 	}
 	else{
-		while(hash->arr[pos].estado != ocupado){
+		while(hash->arr[pos]->estado != ocupado){
 			pos ++;
 		}
 		iter->pos = pos;
@@ -203,7 +234,7 @@ hash_iter_t* hash_iter_crear(const hash_t* hash){
 bool hash_iter_avanzar(hash_iter_t* iter){
 	bool iterar = true;
 	size_t pos = iter->pos;
-	while(iter->hash->arr[pos +1].estado != ocupado && iterar == true){
+	while(iter->hash->arr[pos +1]->estado != ocupado && iterar == true){
 		if(iter->hash->cantidad == 0 || hash_iter_al_final(iter)){
 		iterar = false;
 		}
@@ -218,12 +249,12 @@ bool hash_iter_avanzar(hash_iter_t* iter){
 	return false;
 }
 
-// Devuelve clave actual, esa clave no se puede modificar ni liberar.
+// Devuelve clave actual, esa clave no se puede modificar ni liberar->
 const char* hash_iter_ver_actual(const hash_iter_t* iter){
 	if(hash_iter_al_final(iter)){
 		return NULL;
 	}
-	return iter->hash->arr[iter->pos].clave;
+	return iter->hash->arr[iter->pos]->clave;
 }
 
 // Comprueba si terminó la iteración
