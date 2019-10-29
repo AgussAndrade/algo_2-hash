045139a2e29e4d82fec67ba34d2c086aa892c1be
GabyOrtega <56203290+GabyOrtega@users.noreply.github.com>
2019-10-29 01:48:03 -0300

    Update hash.c

--- hash.c
+++ hash.c
@@ -1,16 +1,3 @@
-#include "hash.h"
-#undef get16bits
-
-#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
-  || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
-#define get16bits(d) (*((const uint16_t *) (d)))
-#endif
-
-#if !defined (get16bits)
-#define get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8)\
-                       +(uint32_t)(((const uint8_t *)(d))[0]) )
-#endif // cosas del hash 
-
 #include <stdbool.h>
 #include <stddef.h>
 #include <limits.h>
@@ -18,6 +5,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include "hash.h"
 
 #define _POSIX_C_SOURCE
 #define LARGO_I 37
@@ -62,53 +50,16 @@ char *copiar_cadena(const char *str){
 	return nueva_cadena;
 }
 
-
-size_t hashing (const char * data, size_t len) {
-size_t hash = len, tmp;
-size_t rem;
-
-    if (len <= 0 || data == NULL) return 0;
-
-    rem = len & 3;
-    len >>= 2;
-
-    /* Main loop */
-    for (;len > 0; len--) {
-        hash  += get16bits (data);
-        tmp    = (get16bits (data+2) << 11) ^ hash;
-        hash   = (hash << 16) ^ tmp;
-        data  += 2*sizeof (uint16_t);
-        hash  += hash >> 11;
-    }
-
-    /* Handle end cases */
-    switch (rem) {
-        case 3: hash += get16bits (data);
-                hash ^= hash << 16;
-                hash ^= ((signed char)data[sizeof (uint16_t)]) << 18;
-                hash += hash >> 11;
-                break;
-        case 2: hash += get16bits (data);
-                hash ^= hash << 11;
-                hash += hash >> 17;
-                break;
-        case 1: hash += (signed char)*data;
-                hash ^= hash << 10;
-                hash += hash >> 1;
-    }
-
-    /* Force "avalanching" of final 127 bits */
-    hash ^= hash << 3;
-    hash += hash >> 5;
-    hash ^= hash << 4;
-    hash += hash >> 17;
-    hash ^= hash << 25;
-    hash += hash >> 6;
-
-    return hash;
+size_t hashing(const char* str, size_t length)
+{
+   size_t hash = 5381;
+   size_t i    = 0;
+   for (i = 0; i < length; ++str, ++i)
+   {
+      hash = ((hash << 5) + hash) + (*str);
+   }
+   return hash;
 }
-
-
 
 bool hash_redimensionar (hash_t* hash,size_t tam){
 	campo_t* datos_nuevo = calloc(3,tam * sizeof(campo_t));
