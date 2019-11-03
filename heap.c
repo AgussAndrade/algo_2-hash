c33d0faea6623e356842871da0c6262a7deab8bb
AgussAndrade <51175036+AgussAndrade@users.noreply.github.com>
2019-11-03 20:43:18 -0300

    Add files via upload

--- heap.c
+++ heap.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include "heap.h"
 #define CAP_I 37
 
 struct heap{
@@ -11,6 +12,94 @@ struct heap{
 	size_t cantidad;
 	size_t capacidad;
 };
+size_t ob_padre(size_t pos){
+	return (pos - 1)/2;
+}
+size_t ob_hijo_i (size_t pos){
+	return 2 * pos + 1;
+}
+size_t ob_hijo_d (size_t pos){
+	return 2*pos+2;
+}
+void swap (void** arr,size_t i,size_t j){
+	void* aux = arr[i];
+	arr[i] = arr[j];
+	arr[j] = aux;
+}
+void upheap (void** arr,cmp_func_t cmp,size_t inicio,size_t act,size_t final){
+	if (act <= inicio || act >= final) return;
+	size_t i = act;
+	while (i > inicio){
+		size_t padre = ob_padre(i);
+		if (padre <inicio) return;
+		if (cmp(arr[i],arr[padre])>0){
+			swap(arr,i,padre);
+			i = padre;
+		}
+		else return;
+	}
+	return;
+}
+void downheap(void** arr,cmp_func_t cmp,size_t inicio,size_t act,size_t final){
+	if (act < inicio || act >= final) return;
+	size_t i = act;
+	while (i < final){
+		size_t hijo_d = ob_hijo_d(i);
+		size_t hijo_i = ob_hijo_i(i);
+		if(hijo_d < final && hijo_i < final){
+			size_t aux = 0; // nunca pasa pero no compila si no
+			if (cmp(arr[hijo_d],arr[hijo_i]) > 0){
+				aux = hijo_d;
+			}
+			else aux = hijo_i;
+			if (cmp(arr[aux],arr[i]) > 0 ){
+				swap(arr,aux,i);
+				i = aux;
+				continue;
+			}
+		}
+		else if (hijo_d < final){		
+			if (cmp(arr[hijo_d],arr[i])>0){
+				swap(arr,i,hijo_d);
+				i = hijo_d;
+				continue;
+			}
+		}
+		else if (hijo_i < final){
+			if (cmp(arr[hijo_i],arr[i])>0){
+				swap(arr,i,hijo_d);
+				i = hijo_i;
+				continue;
+			}
+		}
+		else return;
+
+	}
+	return;	
+}
+
+void heapify (void** arr,size_t cant,cmp_func_t cmp){
+	for (int i = cant;i>=0;i--){
+		downheap(arr,cmp,0,i-1,cant);
+	}
+
+}
+heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
+	heap_t* heap = malloc(sizeof(heap_t));
+	if(!heap) return NULL;
+	void** aux = calloc(n*2,sizeof(void*)*2*n);
+	if (!aux){
+		free(heap);
+		return NULL;
+	}
+	heapify(aux,n,cmp);
+	heap->cantidad = n;
+	heap->capacidad = n*2;
+	heap->arr = aux;
+	heap->cmp = cmp;
+	return heap;
+
+}
 
 heap_t *heap_crear(cmp_func_t cmp){
 	heap_t* heap = malloc(sizeof(heap_t));
@@ -49,99 +138,12 @@ void *heap_ver_max(const heap_t *heap){
 	return heap->arr[0];
 }
 
-size_t padre(size_t pos){
-	return (pos - 1)/2;
-}
-size_t hijo_i (size_t pos){
-	return 2 * pos + 1;
-}
-size_t hijo_d (size_t pos){
-	return 2*pos+2;
-}
-void swap (void** arr,size_t i,size_t j){
-	void* aux = arr[i];
-	arr[i] = arr[j];
-	arr[j] = aux;
-}
-void upheap (void** arr,cmp_func_t cmp,size_t inicio,size_t act,size_t final){
-	if (act <= inicio || act >= final) return;
-	size_t i = act;
-	while (i > inicio){
-		size_t padre = padre(i);
-		if (padre <inicio) return;
-		if (cmp(arr[i],arr[padre])>0){
-			swap(arr,i,padre);
-			i = padre;
-		}
-		else return;
-	}
-	return;
-}
-void downheap(void** arr,cmp_func_t cmp,size_t inicio,size_t act,size_t final){
-	if (act < inicio || act >= final) return;
-	size_t i = act;
-	while (i < final){
-		size_t hijo_d = hijo_d(i);
-		size_t hijo_i = hijo_i(i);
-		if(hijo_d < final && hijo_i < final){
-			size_t aux = 0; // nunca pasa pero no compila si no
-			if (cmp(arr[hijo_d],arr[hijo_i]) > 0){
-				aux = hijo_d;
-			}
-			else aux = hijo_i;
-			if (cmp(arr[aux],arr[i]) > 0 ){
-				swap(arr,aux,i);
-				i = aux;
-				continue;
-			}
-		}
-		else if (hijo_d < final){		
-			if (cmp(arr[hijo_d],arr[i])>0){
-				swap(arr,i,hijo_d);
-				i = hijo_d;
-				continue;
-			}
-		}
-		else if (hijo_i < final){
-			if (cmp(arr[hijo_i],arr[i])>0){
-				swap(arr,i,hijo_d);
-				i = hijo_i;
-				continue;
-			}
-		}
-		else return;
-
-	}
-	return;	
-}
 void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp){
 	heapify(elementos,cant,cmp);
 	for (int i = cant;i>0;i--){
 		swap(elementos,0,i-1);
 		downheap(elementos,cmp,0,0,i);
 	}
-
-}
-void heapify (void** arr,size_t cant,cmp_func_t cmp){
-	for (int i = cant;i>=0;i--){
-		downheap(aux,0,i-1,cant);
-	}
-
-}
-heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
-	heap_t* heap = malloc(sizeof(heap_t));
-	if(!heap) return NULL;
-	void** aux = calloc(n*2,sizeof(void*)*2*n);
-	if (!aux){
-		free(heap);
-		return NULL;
-	}
-	heapify(aux,n,cmp);
-	heap->cantidad = n;
-	heap->capacidad = n*2;
-	heap->arr = aux;
-	heap->cmp = cmp;
-	return heap;
 
 }
 
@@ -162,7 +164,7 @@ bool heap_esta_vacio(const heap_t *heap){
 }
 
 void *heap_desencolar(heap_t *heap){
-	if((heap->capacidad >= CAP_I) && ((pila->capacidad) /4 >= heap->cantidad)){
+	if((heap->capacidad >= CAP_I) && ((heap->capacidad) /4 >= heap->cantidad)){
 		redimensionar(heap,heap->capacidad / 2);
 	}
 	if (heap_esta_vacio(heap)) return NULL;
@@ -170,11 +172,7 @@ void *heap_desencolar(heap_t *heap){
 	swap(heap->arr,0,heap->cantidad -1);
 	heap->cantidad--;
 	heap->arr[heap->cantidad] = NULL;
-	downheap(heap->arr,0,0,heap->cantidad);
+	downheap(heap->arr,heap->cmp,0,0,heap->cantidad);
 	return elem;
 }
 
-void *heap_ver_max(const heap_t *heap){
-	if (heap_esta_vacio(heap)) return NULL;
-	return heap->arr[0];
-}
