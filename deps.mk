71a5a7e46ed70b734cb898823537864a8a296204
AgussAndrade <51175036+AgussAndrade@users.noreply.github.com>
2019-11-09 11:14:21 -0300

    Add files via upload

--- deps.mk
+++ deps.mk
@@ -1,6 +1,6 @@
 CC = gcc
 CFLAGS = -g -O2 -std=c99 -Wall -Werror -pedantic -Wformat=2 -Wshadow -Wpointer-arith -Wconversion -Wunreachable-code -Wno-sign-conversion -Wbad-function-cast -Wtype-limits
-VAL: valgrind --leak-check=full --track-origins=yes --show-reachable=yes
+VAL= valgrind --leak-check=full --track-origins=yes --show-reachable=yes
 heap: heap.c heap.h main.c pruebas_alumno.c testing.c testing.h
 	$(CC) $(CFLAGS) -o heap *.c
 
