6efaf724942e2606d2b00fc0f72df2de28181d5d
GabyOrtega <56203290+GabyOrtega@users.noreply.github.com>
2019-11-13 21:13:26 -0300

    Update heap.c

--- heap.c
+++ heap.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include "heap.h"
 #define CAP_I 37
+#define PORCENTAJE_REDIMENSION 4
 
 struct heap{
 	cmp_func_t cmp;
@@ -12,8 +13,8 @@ struct heap{
 	size_t cantidad;
 	size_t capacidad;
 };
-size_t ob_padre(size_t pos){
-	return (pos - 1)/2;
+int ob_padre(size_t pos){
+	return ((int)pos - 1)/2;
 }
 size_t ob_hijo_i (size_t pos){
 	return 2 * pos + 1;
@@ -28,9 +29,9 @@ void swap (void** arr,size_t i,size_t j){
 }
 void upheap (void** arr,cmp_func_t cmp,size_t inicio,size_t act,size_t final){
 	if (act <= inicio || act >= final) return;
-	size_t i = act;
-	while (i > inicio){
-		size_t padre = ob_padre(i);
+	int i = (int)act;
+	while (i > (int)inicio){
+		int padre = ob_padre(i);
 		if (padre <inicio) return;
 		if (cmp(arr[i],arr[padre])>0){
 			swap(arr,i,padre);
@@ -69,7 +70,7 @@ void downheap(void** arr,cmp_func_t cmp,size_t inicio,size_t act,size_t final){
 		}
 		else if (hijo_i < final){
 			if (cmp(arr[hijo_i],arr[i])>0){
-				swap(arr,i,hijo_d);
+				swap(arr,i,hijo_i);
 				i = hijo_i;
 				continue;
 			}
@@ -87,17 +88,21 @@ void heapify (void** arr,size_t cant,cmp_func_t cmp){
 	}
 
 }
-/*Por algun motivo no puedo liberar el calloc de aca en las pruebas, creo que es de implementación */
+
 heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
 	heap_t* heap = malloc(sizeof(heap_t));
+
 	if(!heap) return NULL;
 	void** aux = calloc(n*2,sizeof(void*)*2*n);
+
 	if (!aux){
 		free(heap);
 		return NULL;
 	}
-	heapify(arreglo,n,cmp);
-	aux = arreglo;
+	for(int i =0; i<n;i++) aux[i] = arreglo[i];
+
+	heapify(aux,n,cmp);
+
 	heap->cantidad = n;
 	heap->capacidad = n*2;
 	heap->arr = aux;
@@ -108,57 +113,68 @@ heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
 
 heap_t *heap_crear(cmp_func_t cmp){
 	heap_t* heap = malloc(sizeof(heap_t));
+
 	if(!heap) return NULL;
 	void** arreglo = calloc(CAP_I,sizeof(void*)* CAP_I);
+
 	if (!arreglo){
 		free(heap);
 		return NULL;
 	}
+
 	heap->cantidad = 0;
 	heap->capacidad = CAP_I;
 	heap->arr = arreglo;
 	heap->cmp = cmp;
 	return heap;
 }
+
 bool redimensionar (heap_t* heap,size_t num){
 	void** arreglo = realloc(heap->arr,sizeof(void*)*num);
 	if (!arreglo) return false;
-	for (size_t i = heap->cantidad;i<num;i++){
-		arreglo[i] = NULL;
-	}
+
 	heap->arr = arreglo;
 	heap->capacidad = num;
 	return true;
 }
+
 size_t heap_cantidad(const heap_t* heap){
 	return heap->cantidad;
 }
+
 bool heap_encolar(heap_t *heap, void *elem){
-	if (heap->cantidad +1 >= heap->capacidad){
+
+	if (heap->cantidad == heap->capacidad){
 		bool rta = redimensionar(heap,heap->capacidad*2);
+
 		if (!rta) return false;
 	}
+
 	heap->arr[heap->cantidad] = elem;
 	upheap(heap->arr,heap->cmp,0,heap->cantidad,heap->cantidad+1);
 	heap->cantidad++;
 	return true;
 }
+
 void *heap_ver_max(const heap_t *heap){
 	return heap->arr[0];
 }
 
 void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp){
 	heapify(elementos,cant,cmp);
-	for (int i = (int)cant;i>0;i--){
-		swap(elementos,0,i-1);
+
+	for (int i = (int)cant-1; i>0; i--){
+		swap(elementos,0,i);
 		downheap(elementos,cmp,0,0,i);
 	}
 }
 
 void heap_destruir(heap_t *heap, void destruir_elemento(void *e)){
 	void** arr = heap->arr;
+
 	if(destruir_elemento){
 		size_t i = 0;
+
 		while(i<heap->cantidad){
 			destruir_elemento(arr[i]);
 			i++;
@@ -169,25 +185,32 @@ void heap_destruir(heap_t *heap, void destruir_elemento(void *e)){
 }
 
 bool heap_esta_vacio(const heap_t *heap){
+
 	if(heap->cantidad == 0) return true;
 	return false;
 }
 
 void *heap_desencolar(heap_t *heap){
-	if((heap->cantidad >= CAP_I) && ((heap->capacidad) /4 >= heap->cantidad)){
+
+	if((heap->capacidad >= CAP_I) && ((heap->capacidad) / PORCENTAJE_REDIMENSION >= heap->cantidad)){
 		redimensionar(heap,heap->capacidad / 2);
 	}
+
 	if (heap_esta_vacio(heap)) return NULL;
+
 	if (heap->cantidad == 1){
 		void* elem = heap->arr[0];
 		heap->arr[0] = NULL;
 		heap->cantidad = 0;
 		return elem;
 	}
+
 	void* elem = heap->arr[0];
 	swap(heap->arr,0,heap->cantidad -1);
 	heap->arr[heap->cantidad -1] = NULL;
-	heap->cantidad--;
+	heap->cantidad --;
 	downheap(heap->arr,heap->cmp,0,0,heap->cantidad);
+
 	return elem;
 }
+
